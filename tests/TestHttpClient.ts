05bc4c4f1a10eb45437a6af063438b3cb34e35b8
Juan Roldan <juan.roldan@leaseplan.com>
2022-07-12 07:33:16 +0200

    Implement tests for request pool

--- tests/TestHttpClient.ts
+++ tests/TestHttpClient.ts
@@ -82,4 +82,38 @@ describe('HttpClient', () => {
       ],
     );
   });
+
+  it('Should request concurrently and handle errors', async () => {
+    jest.setTimeout(10_000);
+    const httpClientConfig = {
+      ...defaultHttpConfig,
+      concurrentRequests: 5,
+      retries: 2,
+      retryDelay: 0.5,
+    };
+    const client = await new HttpClient(httpClientConfig);
+    for (const axiosClient of client.clients) {
+      const mockClient = new MockAdapter(axiosClient);
+      mockClient.onGet('https://mybadurl.com/').reply(200, HTML);
+      mockClient.onGet('https://mybadurl.com/error').replyOnce(404, HTML);
+      mockClient.onGet('https://mybadurl.com/error').replyOnce(404, HTML);
+    }
+
+    const responses = await client.get(
+      [
+        'https://mybadurl.com/',
+        'https://mybadurl.com/error',
+        'https://mybadurl.com/',
+        'https://mybadurl.com/',
+        'https://mybadurl.com/',
+      ],
+    );
+    expect(responses[0].isSuccess).toBeTruthy();
+    expect(responses[0].response).toBeDefined();
+    expect(responses[0].url).toBe('https://mybadurl.com/');
+
+    expect(responses[1].response).toBeUndefined();
+    expect(responses[1].url).toBe('https://mybadurl.com/error');
+    expect(responses[1].isSuccess).toBeFalsy();
+  });
 });
